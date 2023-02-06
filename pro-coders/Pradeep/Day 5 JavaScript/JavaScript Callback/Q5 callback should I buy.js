e29ac0831c474d7a1d81bb0e27cf01804a36fc09
Pradeep Prajapati <mantha747@gmail.com>
2023-02-07 00:50:21 +0530

    javaScript Added

---
+++ pro-coders/Pradeep/Day 5 JavaScript/JavaScript Callback/Q5 callback should I buy.js
@@ -0,0 +1,37 @@
+function conservativeSpender(balance)  
+{
+  return balance > 100
+}
+
+function liberalSpender(balance) 
+{
+  return balance > 10
+}
+
+function horribleSaver (balance) 
+{
+  return balance > 0
+}
+/**
+ * we use amount to give response according to balance.
+ * @param {amount} balance 
+ * @param {callback accordingly} callback 
+ */
+function shouldIBuyThis(balance,callback){
+    let Item = callback(balance)
+
+    if(Item){
+        console.log("Sure! I've got the money!")
+    }
+    else{
+        console.log("Nope! Gotta keep my savings up!")
+    }
+  }
+shouldIBuyThis(20, horribleSaver)
+// logs: "Sure! I've got the money!"
+shouldIBuyThis(20, liberalSpender)
+// logs: "Sure! I've got the money!"
+shouldIBuyThis(20, conservativeSpender)
+// logs:  "Nope! Gotta keep my savings up!"
+shouldIBuyThis(101, conservativeSpender)
+// logs: "Sure! I've got the money!"
