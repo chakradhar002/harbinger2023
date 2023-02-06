6120df825910c251b84f5ec46038ce8368686095
unknown <mantha747@gmail.com>
2023-02-06 16:45:02 +0530

    nodejs

---
+++ Day7/nodejs/Day1/nodedemo/event_demo.js
@@ -0,0 +1,21 @@
+var emitter  = require('events').EventEmitter;
+var em  =  new emitter();
+
+
+
+  // Raising FirstEvent
+  em.emit('firstevent','tup coming MJ ')
+  setTimeout(() => {
+    
+  }, 1000);
+
+  // Raising FirstEvent
+  em.emit('SecondEvent','Brayan adam show')
+  setTimeout(() => {
+    
+  }, 1000);
+
+
+
+
+///
