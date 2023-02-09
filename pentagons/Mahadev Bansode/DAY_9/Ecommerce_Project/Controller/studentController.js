314bb821ce98496b2c52a3eb607a2f7b92ac9500
Mahadev Bansode <Mahadev.Bansode@harbingergroup.com>
2023-02-09 17:01:12 +0530

    Student CRUD Operations

--- pentagons/Mahadev Bansode/DAY_9/Ecommerce_Project/Controller/studentController.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-let router = express.Router();
-const service = require('../Service/getStudentList');
-
-router.get('/getstudentlist',service.getStudentList);
-
-module.exports = router;
